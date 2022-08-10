a2613d5afb5e4b54cb1209a8b30468e9974f2357
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2022-08-11 00:34:16 +0530

    git testing code added

--- Backend/app.js
+++ Backend/app.js
@@ -2,5 +2,6 @@ const express = require('express');
 var app = new express
 var PORT = 3666
 app.listen(PORT,function(){
+    console.log(`hello`);
     console.log(`listening to port ${PORT}`);
   });

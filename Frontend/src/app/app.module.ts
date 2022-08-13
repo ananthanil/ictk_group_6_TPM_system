419a15057c14ff45c96682ef364a0730b88eb85b
Nesla0524 <neslanes0524@gmail.com>
2022-08-13 17:46:13 +0530

    Add signin and forgot-psw  page

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { FinanceHomepageComponent } from './finance-homepage/finance-homepage.component';
+import { SigninComponent } from './signin/signin.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    FinanceHomepageComponent
+    FinanceHomepageComponent,
+    SigninComponent,
+    ForgotPasswordComponent,
+   
+   
   ],
   imports: [
     BrowserModule,

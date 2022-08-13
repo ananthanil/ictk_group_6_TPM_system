501ed3bdcee4c3fcfecdb46e598de245e881bf25
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2022-08-13 20:42:16 +0530

    added router links

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
@@ -7,7 +8,8 @@ import { SignupComponent } from './signup/signup.component';
 const routes: Routes = [
   {path:'',component: HomeComponent},
   {path:'signin',component:SigninComponent},
-  {path:'signup',component:SignupComponent}
+  {path:'signup',component:SignupComponent},
+  {path:'forgot',component:ForgotPasswordComponent}
 ];
 
 @NgModule({

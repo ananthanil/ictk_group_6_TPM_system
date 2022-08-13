50b2552f3441592fca1af9a4d80b7910cf40d16f
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2022-08-13 20:15:14 +0530

    removed error

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,34 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< Updated upstream
+
 import { SignupComponent } from './signup/signup.component';
 import { FinanceHomepageComponent } from './finance-homepage/finance-homepage.component';
 import { SigninComponent } from './signin/signin.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
-
-=======
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
->>>>>>> Stashed changes
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< Updated upstream
     SignupComponent,
     FinanceHomepageComponent,
     SigninComponent,
     ForgotPasswordComponent,
-   
-   
-=======
     HeaderComponent,
     FooterComponent,
     HomeComponent
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,

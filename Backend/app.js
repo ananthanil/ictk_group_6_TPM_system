efab0dc6f997d875f1a071cb295380e2bb21bf68
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2022-08-18 00:10:32 +0530

    db connection actictied

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 var app = new express
+const category = require('./src/model/trainer_category')
+const cors = require('cors');
+const path = require('path');
 var PORT = 3666
 app.listen(PORT,function(){
     console.log(`listening to port ${PORT}`);

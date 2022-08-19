836aa16dab22663891a22a72c28af9bd246587bd
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2022-08-19 13:07:27 +0530

    testing changes

--- Backend/src/router/adminRoute.js
+++ Backend/src/router/adminRoute.js
@@ -3,16 +3,6 @@
 
 // const adminRouter = express.Router();
 
-// adminRouter.post('/api/add',function(req,res){
-//     console.log(Hai);
-//     console.log(req.body);
-//     var type = {       
-//         trainerCategory : req.body.trainerCategory
-//       }       
-//    var type = new categoryCollection(type);
-//    type.save();
-//   });
-
 // adminRouter.post('/add',function(req,res){
 //     console.log(req.body);
 //     var ttype = {       

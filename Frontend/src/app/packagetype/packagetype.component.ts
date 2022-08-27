9bafd2bed206045e332e6e376a193d5b0b2cbefd
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2022-08-27 12:08:48 +0530

    Dropdown value is added

--- Frontend/src/app/packagetype/packagetype.component.ts
+++ Frontend/src/app/packagetype/packagetype.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../admin.service';
+import { FormGroup, FormControl, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-packagetype',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PackagetypeComponent implements OnInit {
 
-  constructor() { }
+  CategoryList: any = []
+
+  form = new FormGroup({
+    Category: new FormControl('', Validators.required)
+  });
+ 
+  constructor(private adminservice:AdminService) { }
 
   ngOnInit(): void {
+    this.adminservice.getcategories()
+    .subscribe({
+      next: (data)=>{
+        this.CategoryList = JSON.parse(JSON.stringify(data));
+      },
+      error: (err)=> {
+        console.log(err);
+      }
+    })
+    console.log(this.CategoryList)
   }
 
 }

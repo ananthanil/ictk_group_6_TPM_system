3200a51766d97e55581e1f50f497c844531e97ec
Nesla0524 <neslanes0524@gmail.com>
2022-08-28 12:34:05 +0530

    trainer mode updated

--- Frontend/src/app/admin.service.ts
+++ Frontend/src/app/admin.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CategoryModel } from './t-type/category.model';
+import { TrainerMode } from './t-mode/trainermode.model';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -38,4 +40,32 @@ export class AdminService {
     return this.http.put<any>(`${this.server_address}/admin/CategoryRemove/`,Categoryid)
     .subscribe(data => {console.log(data)})
   }
+
+
+  //Trainer mode operationS
+  addTrainerMode(tmode:TrainerMode )
+  {
+    console.log('in service');
+    return this.http.post<any>(`${this.server_address}/admin/trainermodeadd`,{"trMode":tmode})
+    .subscribe((data) => {
+      console.log(data);
+    });
   }
+  gettrainerMode(){
+    return this.http.get<any[]>(`${this.server_address}/admin/TrainerModeview`);
+  }
+  getTrainerModeUpdate(id:any){
+    return this.http.get<any>(`${this.server_address}/admin/`+id);
+  }
+
+  updateTrainerMode(Tmodeid:any){
+    return this.http.put<any>(`${this.server_address}/admin/TrainermodeUpdate/`,Tmodeid)
+    .subscribe(data => {console.log(data)})
+  }
+
+  removeTrainerMode(Tmodeid:any){
+    return this.http.put<any>(`${this.server_address}/admin/TrainermodeRemove/`,Tmodeid)
+    .subscribe(data => {console.log(data)})
+  }
+  }
+  

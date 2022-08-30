24d35a3fa25bf15a3f2729baec3c2f8ecc985dce
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2022-08-30 13:09:05 +0530

    ts file comment removed

--- Frontend/src/app/packagetype/packagetype.component.ts
+++ Frontend/src/app/packagetype/packagetype.component.ts
@@ -10,37 +10,8 @@ import { PackageModel } from './package.model';
 })
 export class PackagetypeComponent implements OnInit {
 
-  // hello : String = '';
-  // hai : String = '';
-  // tata : String = '';
   CategoryList: any = []
   ActivityList: any = []
- 
-
-  // changeCat(e:any){
-  //   this.hello = e.target.value;
-  //   console.log(this.hello);
-  // }
-  // changeCate(e:any){
-  //   console.log(e.target.value)
-  //   this.hai = e.target.value;
-  //   console.log(this.hai);
-  // }
-  // changeActivity(e:any){
-  //   this.tata = e.target.value;
-  //   console.log(this.tata);
-  // }
-  // hello: any;
-  // hai:any
-
-  // form = new FormGroup({
-  //   Category: new FormControl('', Validators.required),
-  //   Category1: new FormControl('', Validators.required),
-  //   Activity: new FormControl('', Validators.required),
-  //   Hour_Amount: new FormControl('', Validators.required),
-  //   per_head_amount: new FormControl('', Validators.required),
-  //   satus  : new FormControl(1, Validators.required),
-  // });
 
   insert = new PackageModel('','','',0,0,1);
 

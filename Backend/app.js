ca11e750303b1b9f9e63268d1894256c0c2be95f
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2022-08-31 00:29:42 +0530

    added router file connections to the app.js

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 var bodyparser = require('body-parser');
 const adminRouter = require('./src/router/adminRoute')
 const signupRouter = require('./src/router/signupRouter')
+const trainerRouter = require('./src/router/trainerRouter')
 var app = new express();
 var PORT = 3666
 app.use(cors());
@@ -10,6 +11,7 @@ app.use(bodyparser.json());
 
 app.use('/api/admin/',adminRouter);
 app.use('/api/user', signupRouter);
+app.use('/api/trainer/', trainerRouter);
 
 app.listen(PORT,function(){
     console.log(`listening to port ${PORT}`);
